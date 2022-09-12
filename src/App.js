02f19f3b558ab6b36cb22055d572dc222162183f
bhavyakukkar <nigelwithrow78@gmail.com>
2022-09-12 23:44:29 +0530

    V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import ShareWidget from './components/ShareWidget';
+
+
+//Called every time page is shared
+function handleStoreShareData({ appName }) {
+    /*
+    Following object can be pushed to the server:
+    const time = new Date().getTime();
+
+    {
+        time, eg: 1663004372159
+        appName, eg: 'whatsapp'
+    }
+    */
+
+    //Message on page once user returns after sharing
+    document.querySelector("#message").innerHTML = 'Thank you for sharing!';
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+
+            {/* Rest of the Website */}
+            <div className='page'>
+                <p id="message"></p>
+            </div>
+    
+            {/* Widget: Add Social */}
+            <div className='share'>
+                <ShareWidget
+                    storeShareData={handleStoreShareData}
+                />
+            </div>
+
+        </React.Fragment>
+    );
 }
 
 export default App;
